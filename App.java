42a170d2d7460b3bd130b00aa718e52e8a4c6204
Kaki Mamatha <135447220+mamathakaka@users.noreply.github.com>
2023-06-23 12:42:33 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package automation1.testcallsign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
